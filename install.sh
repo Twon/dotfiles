1d91b234389968f0d6475d653344e215b539c68d
Antony Peacock <ant.peacock@gmail.com>
2021-02-17 17:30:35 +0000

    Git ignore file

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ if [ ! -f ~/.lldbinit ]; then
     echo ".lldbinit not found!  Creating symbolic link"
     ln -s $DIR/.lldbinit ~/.lldbinit
 fi
+
+if [ ! -f ~/.gitconfig ]; then
+    echo ".gitconfig not found!  Creating symbolic link"
+    ln -s $DIR/.gitconfig ~/.gitconfig
+fi
