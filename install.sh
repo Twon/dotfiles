6e8ce3861701ba6849f942328696611f18ed74a6
Antony Peacock <ant.peacock@gmail.com>
2023-05-06 15:47:52 +0100

    Update Linux install script

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Script continues here if running as root
+script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+folders=("$script_path" "$script_path/os/linux")
+destination_folder="$HOME"
 
-files = "
-.lldbinit
-.gitconfig
-.profile
-.vimrc
-"
-
-for file in $files
-do
-    if [ ! -f ~/$file ]; then
-        echo "$file not found!  Creating symbolic link"
-        ln -s $DIR/$file ~/$file
+for folder in "${folders[@]}"; do
+  for file in "$folder"/*; do
+    if [[ -f "$file" ]]; then
+      target="$destination_folder/$(basename "$file")"
+      if [[ -L "$target" ]]; then
+        echo "Symbolic link '$(basename "$file")' already exists in the destination folder."
+      elif [[ -e "$target" ]]; then
+        echo "File '$(basename "$file")' already exists in the destination folder."
+      else
+        ln -s "$file" "$target"
+        echo "Created symbolic link for $(basename "$file") in the root of the destination folder."
+      fi
     fi
+  done
 done
-
